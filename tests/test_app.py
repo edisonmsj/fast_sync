a67886658225f573e54443c22c9477f6aa2af01b
Edison Motta santos Júnior <edison.msj@gmail.com>
2024-06-16 21:13:16 -0300

    test: add html response test

--- tests/test_app.py
+++ tests/test_app.py
@@ -13,3 +13,20 @@ def test_root_deve_retornar_ok_e_ola_mundo():
     assert response.status_code == HTTPStatus.OK
     assert response.json() == {'message': 'Olá Mundo!'}
 
+
+def test_root_deve_retornar_ok_e_html():
+    client = TestClient(app)
+
+    response = client.get('/')
+
+    assert response.status_code == HTTPStatus.OK
+
+    content_type = response.headers.get('Content-Type')
+    assert 'text/html' in content_type, 'Content-Type is not HTML'
+
+    assert (
+        '<html' in response.text.lower()
+    ), 'Response does not contain <html> tag'
+    assert (
+        '</html>' in response.text.lower()
+    ), 'Response does not contain </html> tag'
