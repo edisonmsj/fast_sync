336707489e129e2be15a76dad8491ff50627444b
Edison Motta santos Júnior <edison.msj@gmail.com>
2024-06-20 20:16:40 -0300

    feat: user post

--- fast_zero/app.py
+++ fast_zero/app.py
@@ -1,6 +1,9 @@
+from http import HTTPStatus
+
 from fastapi import FastAPI
 from fastapi.responses import HTMLResponse
-#from fast_zero.schemas import Message
+
+from fast_zero.schemas import UserDB, UserPublic, UserSchema
 
 app = FastAPI()
 
@@ -45,3 +48,15 @@ def read_root():
     </body>
     </html>
     """
+
+
+database = []
+
+
+@app.post('/users/', status_code=HTTPStatus.CREATED, response_model=UserPublic)
+def create_user(user: UserSchema):
+    user_with_id = UserDB(**user.model_dump(), id=len(database) + 1)
+
+    database.append(user_with_id)
+
+    return user_with_id
--- tests/test_app.py
+++ tests/test_app.py
@@ -5,16 +5,7 @@ from fastapi.testclient import TestClient
 from fast_zero.app import app
 
 
-def test_root_deve_retornar_ok_e_ola_mundo():
-    client = TestClient(app)
-
-    response = client.get('/')
-
-    assert response.status_code == HTTPStatus.OK
-    assert response.json() == {'message': 'Olá Mundo!'}
-
-
-def test_root_deve_retornar_ok_e_html():
+def test_root_must_return_ok_e_html():
     client = TestClient(app)
 
     response = client.get('/')
@@ -30,3 +21,22 @@ def test_root_deve_retornar_ok_e_html():
     assert (
         '</html>' in response.text.lower()
     ), 'Response does not contain </html> tag'
+
+
+def test_create_user():
+    client = TestClient(app)
+
+    response = client.post(
+        '/users/',
+        json={
+            'username': 'alice',
+            'email': 'alice@example.com',
+            'password': 'secret',
+        },
+    )
+    assert response.status_code == HTTPStatus.CREATED
+    assert response.json() == {
+        'username': 'alice',
+        'email': 'alice@example.com',
+        'id': 1,
+    }
