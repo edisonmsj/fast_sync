0d58abf9142c2bb51577b37e8bb66ae6dc1f76fc
Edison Motta santos Júnior <edison.msj@gmail.com>
2024-06-23 20:44:11 -0300

    feat: add get one user info by id

--- fast_zero/app.py
+++ fast_zero/app.py
@@ -67,6 +67,11 @@ def read_users():
     return {'users': database}
 
 
+@app.get('/users/{id}', status_code=HTTPStatus.OK, response_model=UserPublic)
+def read_user(id: int):
+    return database[id - 1]
+
+
 @app.put('/users/{user_id}', response_model=UserPublic)
 def update_user(user_id: int, user: UserSchema):
     if user_id > len(database) or user_id < 1:
