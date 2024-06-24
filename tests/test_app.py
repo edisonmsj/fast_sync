6dd7e47f287888b1d5cbabb402e399ba0497c552
Edison Motta santos Júnior <edison.msj@gmail.com>
2024-06-23 21:08:46 -0300

    feat: add test for bad put and delete

--- tests/test_app.py
+++ tests/test_app.py
@@ -80,3 +80,24 @@ def test_delete_user(client):
 
     assert response.status_code == HTTPStatus.OK
     assert response.json() == {'message': 'User deleted'}
+
+
+def test_update_invalid_user(client):
+    response = client.put(
+        '/users/2',
+        json={
+            'username': 'bob',
+            'email': 'bob@example.com',
+            'password': 'mynewpassword',
+        },
+    )
+
+    assert response.status_code == HTTPStatus.NOT_FOUND
+    assert response.json() == {'detail': 'User not found'}
+
+
+def test_delete_invalid_user(client):
+    response = client.delete('/users/2')
+
+    assert response.status_code == HTTPStatus.NOT_FOUND
+    assert response.json() == {'detail': 'User not found'}
