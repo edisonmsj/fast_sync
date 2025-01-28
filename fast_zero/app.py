57633a2259cea6b171f003ffdd2853b2e8fa8a51
Edison <edison.msj@gmail.com>
2025-01-28 20:43:04 -0300

    feat: refactor to auth route

--- fast_zero/app.py
+++ fast_zero/app.py
@@ -1,20 +1,11 @@
-from http import HTTPStatus
-
-from fastapi import Depends, FastAPI, HTTPException
+from fastapi import FastAPI
 from fastapi.responses import HTMLResponse
-from fastapi.security import OAuth2PasswordRequestForm
-from sqlalchemy import select
-from sqlalchemy.orm import Session
 
-from fast_zero.database import get_session
-from fast_zero.models import User
-from fast_zero.schemas import Token
-from fast_zero.security import (
-    create_access_token,
-    verify_password,
-)
+from fast_zero.routers import auth, users
 
 app = FastAPI()
+app.include_router(users.router)
+app.include_router(auth.router)
 
 
 @app.get('/', response_class=HTMLResponse)
@@ -57,27 +48,3 @@ def read_root():
     </body>
     </html>
     """
-
-
-@app.post('/token', response_model=Token)
-def login_for_access_token(
-    # O depends vazio indica que o tipo precisa ser respeitado
-    form_data: OAuth2PasswordRequestForm = Depends(),
-    session: Session = Depends(get_session),
-):
-    user = session.scalar(
-        select(User).where(User.username == form_data.username)
-    )
-    if not user:
-        raise HTTPException(
-            status_code=HTTPStatus.UNAUTHORIZED,
-            detail='Incorrect username or password',
-        )
-    if not verify_password(form_data.password, user.password):
-        raise HTTPException(
-            status_code=HTTPStatus.UNAUTHORIZED,
-            detail='Incorrect username or password',
-        )
-    access_token = create_access_token({'sub': user.username})
-
-    return {'access_token': access_token, 'token_type': 'Bearer'}
