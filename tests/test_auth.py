70f2f8eb66c352353cf3a6f123f358fafe50096b
Edison <edison.msj@gmail.com>
2025-02-10 17:38:09 -0300

    test: token expire time

--- tests/test_auth.py
+++ tests/test_auth.py
@@ -1,5 +1,7 @@
 from http import HTTPStatus
 
+from freezegun import freeze_time
+
 
 def test_get_token(client, user):
     response = client.post(
@@ -12,3 +14,27 @@ def test_get_token(client, user):
     assert response.status_code == HTTPStatus.OK
     assert token['token_type'] == 'Bearer'
     assert token['access_token']
+
+
+def test_token_expired_after_time(client, user):
+    with freeze_time('2021-01-01 00:00:00'):
+        response = client.post(
+            '/auth/token',
+            data={'username': user.username, 'password': user.clean_password},
+        )
+
+        assert response.status_code == HTTPStatus.OK
+        token = response.json()['access_token']
+
+    with freeze_time('2021-01-01 00:31:00'):
+        response = client.put(
+            '/users/{user.id}',
+            headers={'Authorization': f'Bearer {token}'},
+            json={
+                'username': 'bob',
+                'email': 'wrong@wrong.com',
+                'password': 'wrong'}
+        )
+
+        assert response.status_code == HTTPStatus.UNAUTHORIZED
+        assert response.json() == {'detail': 'Could not validate credentials'}
