b3ddc44ea8f441ceec38a7a43c2834d3046a304a
Edison <edison.msj@gmail.com>
2025-02-14 17:29:10 -0300

    test: refresh token

--- tests/test_auth.py
+++ tests/test_auth.py
@@ -59,3 +59,35 @@ def test_token_wrong_user(client, user):
 
     assert response.status_code == HTTPStatus.UNAUTHORIZED
     assert response.json() == {'detail': 'Incorrect username or password'}
+
+
+def test_refresh_token(client, token):
+    response = client.post(
+        '/auth/refresh_token', headers={'Authorization': f'Bearer {token}'}
+    )
+
+    data = response.json()
+
+    assert response.status_code == HTTPStatus.OK
+    assert data['token_type'] == 'Bearer'
+    assert 'access_token' in data
+    assert 'token_type' in data
+
+
+def test_token_expired_dont_refresh(client, user):
+    with freeze_time('2021-01-01 00:00:00'):
+        response = client.post(
+            '/auth/token',
+            data={'username': user.username, 'password': user.clean_password},
+        )
+
+        assert response.status_code == HTTPStatus.OK
+        token = response.json()['access_token']
+
+    with freeze_time('2021-01-01 00:31:00'):
+        response = client.post(
+            '/auth/refresh_token', headers={'Authorization': f'Bearer {token}'}
+        )
+
+        assert response.status_code == HTTPStatus.UNAUTHORIZED
+        assert response.json() == {'detail': 'Could not validate credentials'}
