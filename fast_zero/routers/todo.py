3ddbde25301906cb2a414430808e59ac9942f365
Edison <edison.msj@gmail.com>
2025-02-16 11:44:38 -0300

    feat: persist todo on db

--- fast_zero/routers/todo.py
+++ fast_zero/routers/todo.py
@@ -4,7 +4,7 @@ from fastapi import APIRouter, Depends
 from sqlalchemy.orm import Session
 
 from fast_zero.database import get_session
-from fast_zero.models import User
+from fast_zero.models import User, Todo
 from fast_zero.schemas import TodoPublic, TodoSchema
 from fast_zero.security import get_current_user
 
@@ -16,4 +16,15 @@ T_User = Annotated[User, Depends(get_current_user)]
 
 @router.post('/', response_model=TodoPublic)
 def create_todo(todo: TodoSchema, user: T_User, session: T_Session):
+    db_todo = Todo(
+        title=todo.title,
+        description=todo.description,
+        state=todo.state,
+        user_id=user.id,
+    )
+
+    session.add(db_todo)
+    session.commit()
+    session.refresh(db_todo)
+
     return todo
