6010963a1177c98faa8875c262aac54b9727f708
Edison <edison.msj@gmail.com>
2025-02-18 18:40:18 -0300

    feat: test run on postgres container

--- tests/conftest.py
+++ tests/conftest.py
@@ -4,7 +4,7 @@ import pytest
 from fastapi.testclient import TestClient
 from sqlalchemy import create_engine
 from sqlalchemy.orm import Session
-from sqlalchemy.pool import StaticPool
+from testcontainers.postgres import PostgresContainer
 
 from fast_zero.app import app
 from fast_zero.database import get_session
@@ -43,31 +43,17 @@ def client(session):
     app.dependency_overrides.clear()
 
 
-@pytest.fixture()
+@pytest.fixture(scope='function')
 def session():
-    #    create_engine('sqlite:///:memory:'): cria um mecanismo de banco de
-    #    dados SQLite em memória usando SQLAlchemy. Este mecanismo
-    #    será usado para criar uma sessão de banco de dados para nossos
-    #     testes.
-    engine = create_engine(
-        'sqlite:///:memory:',
-        connect_args={'check_same_thread': False},
-        poolclass=StaticPool,
-    )
-    #    table_registry.metadata.create_all(engine): cria todas as tabelas no
-    #    banco de dados de teste antes de cada teste
-    #     usa a fixture session.
-    table_registry.metadata.create_all(engine)
+    with PostgresContainer('postgres:17', driver='psycopg') as postgres:
+        engine = create_engine(postgres.get_connection_url())
+        table_registry.metadata.create_all(engine)
 
-    #    Session(engine): cria uma sessão Session para que os testes possam se
-    #     comunicar com o banco de dados. Por conta do
-    #     yield a sessão é sempre renovada após cada teste.
-    with Session(engine) as session:
-        #    yield Session(): fornece uma instância de Session que será
-        #    injetada em cada teste que solicita a fixture session. Essa
-        #    sessão será usada para interagir com o banco de dados de teste.
-        yield session
-    table_registry.metadata.drop_all(engine)
+        with Session(engine) as session:
+            yield session
+            session.rollback()
+
+        table_registry.metadata.drop_all(engine)
 
 
 @pytest.fixture()
